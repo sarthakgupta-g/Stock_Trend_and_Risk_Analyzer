60feb157975b0842352bc981e5fdace30035930e
Sarthak Gupta <sarthakgupta@umass.edu>
2026-09-02 14:33:57 -0400

    created graph to compre prices and rolling averages over 60 day period

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 data=pd.read_csv("stock_data.csv")
 data.head(5)
@@ -8,3 +9,12 @@ data.info()
 data["MA5"]=data["Price"].rolling(5).mean()
 data["MA20"]=data["Price"].rolling(20).mean()
 data[["Date","Price","MA5","MA20"]].tail(10)
+
+plt.plot(data["Date"],data["Price"],label="Price")
+plt.plot(data["Date"],data["MA5"],label="MA5")
+plt.plot(data["Date"],data["MA20"],label="MA20")
+plt.xlabel("Date")
+plt.ylabel("Price and Rolling Averages (Dollars)")
+plt.title("Changes in Price and Rolling Averages over 60 Days")
+plt.legend()
+plt.show
